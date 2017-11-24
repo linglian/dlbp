53c3847e65a782c61f67686cd70a2ab196426f03
linglian <linglian@github.com>
2017-11-24 23:57:16 +0800

    fineture

--- tools/fine_tune/fine-tune.py
+++ tools/fine_tune/fine-tune.py
@@ -1,79 +1,75 @@
-# Licensed to the Apache Software Foundation (ASF) under one
-# or more contributor license agreements.  See the NOTICE file
-# distributed with this work for additional information
-# regarding copyright ownership.  The ASF licenses this file
-# to you under the Apache License, Version 2.0 (the
-# "License"); you may not use this file except in compliance
-# with the License.  You may obtain a copy of the License at
-#
-#   http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing,
-# software distributed under the License is distributed on an
-# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
-# KIND, either express or implied.  See the License for the
-# specific language governing permissions and limitations
-# under the License.
+#coding=utf-8
 
 import os
 import argparse
 import logging
 import sys
-logging.basicConfig(level=logging.DEBUG)
-sys.path.append('/home/lol/dl/dlbp/tools/fine_tune/common')
-import data, fit, modelzoo
-sys.path.insert(0, '/home/lol/dl/mxnet/python')
-import mxnet as mx
+logging.basicConfig(level=logging.INFO)
 
-def get_fine_tune_model(symbol, arg_params, num_classes, layer_name):
-    """
-    symbol: the pre-trained network symbol
-    arg_params: the argument parameters of the pre-trained model
-    num_classes: the number of classes for the fine-tune datasets
-    layer_name: the layer name before the last fully-connected layer
-    """
-    all_layers = symbol.get_internals()
-    net = all_layers[layer_name+'_output']
-    net = mx.symbol.FullyConnected(data=net, num_hidden=num_classes, name='fc')
-    net = mx.symbol.SoftmaxOutput(data=net, name='softmax')
-    new_args = dict({k:arg_params[k] for k in arg_params if 'fc' not in k})
-    return (net, new_args)
+path = '/home/lol/dl/Image'
+prefix = "full-resnet-152"
+num_round = 0
+num_epoch = 5
+mxnetPath = '/home/lol/dl/mxnet/python'
+if __name__ == '__main__':
+    import getopt
+    opts, args = getopt.getopt(sys.argv[1:], 'f:x:p:r:e:')
+    for op, value in opts:
+        if op == '-f':
+            path = value
+        elif op == '-x':
+            mxnetPath = value
+            sys.path.insert(0, '/home/lol/dl/mxnet/python')
+        elif op == '-p':
+            prefix = value
+        elif op == '-r':
+            num_round = value
+        elif op == '-e':
+            num_epoch = value
+
+    sys.path.insert(0, mxnetPath)
+    import mxnet as mx
+    model = mx.model.FeedForward.load(
+        prefix, epoch=num_round, ctx=mx.gpu(0), numpy_batch_size=1)
+    internals = model.symbol.get_internals()
 
 
-if __name__ == "__main__":
-    # parse args
-    parser = argparse.ArgumentParser(description="fine-tune a dataset",
-                                     formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    train = fit.add_fit_args(parser)
-    data.add_data_args(parser)
-    aug = data.add_data_aug_args(parser)
-    parser.add_argument('--pretrained-model', type=str,
-                        help='the pre-trained model')
-    parser.add_argument('--layer-before-fullc', type=str, default='flatten0',
-                        help='the name of the layer before the last fullc layer')
-    # use less augmentations for fine-tune
-    data.set_data_aug_level(parser, 1)
-    # use a small learning rate and less regularizations
-    parser.set_defaults(image_shape='3,224,224', num_epochs=30,
-                        lr=.01, lr_step_epochs='20', wd=0, mom=0)
+    data = internals["pool1_output"]
 
-    args = parser.parse_args()
+    print data
 
-    # load pretrained model
-    dir_path = os.path.dirname(os.path.realpath(__file__))
-    (prefix, epoch) = modelzoo.download_model(
-        args.pretrained_model, os.path.join(dir_path, 'model'))
-    if prefix is None:
-        (prefix, epoch) = (args.pretrained_model, args.load_epoch)
-    sym, arg_params, aux_params = mx.model.load_checkpoint(prefix, epoch)
+    fc1 = mx.symbol.FullyConnected(data=data, num_hidden=512, name='f1')
 
-    # remove the last fullc layer
-    (new_sym, new_args) = get_fine_tune_model(
-        sym, arg_params, args.num_classes, args.layer_before_fullc)
+    relu1 = mx.sym.Activation(data=fc1, name='relu1', act_type="relu")
 
-    # train
-    fit.fit(args        = args,
-            network     = new_sym,
-            data_loader = data.get_rec_iter,
-            arg_params  = new_args,
-            aux_params  = aux_params)
+    fc2 = mx.symbol.FullyConnected(data=fc1, num_hidden=8, name='f2')
+
+    softmax = mx.symbol.SoftmaxOutput(data=fc2, name='softmax')
+
+    model = mx.model.FeedForward(ctx=mx.gpu(0), symbol=softmax, numpy_batch_size=1,
+                                                arg_params=model.arg_params, aux_params=model.aux_params, allow_extra_params=True)
+
+    model.num_epoch = num_epoch
+
+    data_shape = (3, 224, 224)  
+
+    train = mx.io.ImageRecordIter(  
+        path_imgrec = 'train.rec',      
+        data_shape  = data_shape,  
+        batch_size  = 1,  
+        rand_crop   = True,  
+        rand_mirror = True)  
+
+    val = mx.io.ImageRecordIter(  
+        path_imgrec = 'test.rec',  
+        rand_crop   = False,  
+        rand_mirror = False,  
+        data_shape  = data_shape,  
+        batch_size  = 1)
+
+    model.fit(
+        X = train,
+        eval_data = val)
+
+    model.save('full-resnet-152')
+    model.symbol.save('full-resnet-152')
